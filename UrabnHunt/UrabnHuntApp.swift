eb47c844fa3022464100c0de0ae3155cf88fc7d5
Dzianis Monich <dzianis.monich@sap.com>
2026-03-31 15:34:54 +0200

    Initial Commit

---
+++ UrabnHunt/UrabnHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrabnHuntApp.swift
+//  UrabnHunt
+//
+//  Created by Monich, Dzianis on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct UrabnHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
